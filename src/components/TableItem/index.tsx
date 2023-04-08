1ff14ec39014ec87db3d1e1be6f53ca488a03daf
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-08 14:14:24 -0300

    Update index.tsx

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,12 +1,15 @@
 import * as C from './styles'
 import { Item } from '../../types/item'
 import { formatDate } from '../../helpers/dateFilter'
+import { Category } from '../../types/Category'
 
 type Props = {
     item: Item
 }
 
-export const TableItem = ({item}: Props) => {
+
+
+export const TableItem = ({ item }: Props) => {
     return(
         <C.TableLine>
             <C.TableColumn>{formatDate(item.date)}</C.TableColumn>
