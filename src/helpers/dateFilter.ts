584c3278ff84e10738d69833e35ee1172fcb7c69
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-08 14:14:20 -0300

    Update dateFilter.ts

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -6,7 +6,7 @@ export const getCurrentMonth = () => {
 }
 
 export const filterListByMonth = (list: Item[], date: string):Item[] => {
-    let newList: Item[] = []
+    let newList: Item[] = [];
     let [year, month] = date.split('-')   
 
     for(let i in list) {
@@ -25,9 +25,7 @@ export const formatDate = (date: Date): string =>{
     let year = date.getFullYear()
     let month = date.getMonth() + 1
     let day = date.getDate()
-
-    console.log(day)
-    
-
-    return `${day}-${month}-${year}`
+    return `${addZeroToDate(day)}-${addZeroToDate(month)}-${year}`
 }
+
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`
