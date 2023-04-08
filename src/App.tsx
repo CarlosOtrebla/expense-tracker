79a790045e4ca0085d8c25af4645e8b10fd96e97
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-08 14:14:16 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ import { TableArea } from './components/TableArea'
 
 
 const App = () => {
-  const [list, setList] = useState(items)
-  const [filteredList, setFilteredList] = useState<Item[]>([])
-  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth)
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth))
@@ -29,7 +29,7 @@ const App = () => {
 
         {/* {Area de inserção} */}
 
-        <TableArea list={filteredList}/>
+        <TableArea/>
 
       </C.Body>
     </C.Container>
